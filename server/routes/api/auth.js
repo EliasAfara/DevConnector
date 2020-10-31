a91186aa6e43448974199c988d366f80be2b673f
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-01 01:37:13 +0200

    User Authentication / Login Route

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -2,12 +2,93 @@
 
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const auth = require('../../middleware/auth');
+// By adding auth we can make the route protected
+
+const User = require('../../models/User');
 
 /**
  * @route    #reqtype: GET | #endpoint: api/auth
  * @desc     test route
  * @access   Public (No token needed)
  */
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('500 Internal server error');
+  }
+});
+
+/**
+ * @route    #requesttype: POST | #endpoint: api/auth
+ * @desc     Authenticate user & get token
+ * @access   Public
+ */
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // 400 Bad Request
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user has an account
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({
+          errors: [{ msg: 'Invalid Credentials' }],
+        });
+      }
+
+      // Validate user credentials
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Return jsonwebtoken (when user registers, he will be logged in instantly and that get done using jsonwebtoken)
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('500 Internal Server Error');
+    }
+  }
+);
 
 module.exports = router;
