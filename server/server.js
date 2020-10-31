302165ce32c7350e79df8d72b0593afd2914bf15
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-10-31 18:08:23 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API RUNNING'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
