3bd5efbbd0cf38239025be796c1f0c5ca6b7777b
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-10-31 23:40:44 +0200

    User Registration + JSON Web Token

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -1,8 +1,15 @@
 // handle registering users and adding users
 
 const express = require('express');
+const gravatar = require('gravatar');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
+//const normalize = require('normalize-url');
+
+const User = require('../../models/User'); // User Schema
 
 /**
  * @route    #requesttype: POST | #endpoint: api/users
@@ -19,15 +26,72 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      // 400 Bad Request
       return res.status(400).json({
-        // 400 Bad Request
         errors: errors.array(),
       });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user already exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200', // image size
+        r: 'pg', // no 18+ images
+        d: 'mm', // default image
+      });
+
+      // creates a new user instance (does not save)
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // create salt to do hashing
+      const salt = await bcrypt.genSalt(10);
+
+      // Encrypt password with bycript and store it in user password
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save user to database
+      await user.save();
+
+      // Return jsonwebtoken (when user registers, he will be logged in instantly and that get done using jsonwebtoken)
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('500 Internal Server Error');
+    }
   }
 );
 
