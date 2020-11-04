65b6d012e28cca846f6a9f3eae6913dd5ebdb101
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-04 21:09:16 +0200

    Add Post Route

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -2,12 +2,84 @@
 
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
+/**
+ * @route    #reqtype: POST | #endpoint: api/posts
+ * @desc     test route
+ * @access   Private
+ */
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name, // comes from user
+        avatar: user.avatar,
+        user: req.user.id, // we want user id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('500 Internal server error');
+    }
+  }
+);
 
 /**
  * @route    #reqtype: GET | #endpoint: api/posts
  * @desc     test route
  * @access   Public (No token needed)
  */
-router.get('/', (req, res) => res.send('Posts route'));
+router.get('/', async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('500 Internal server error');
+  }
+});
+
+/**
+ * @route    #reqtype: PUT | #endpoint: api/posts
+ * @desc     test route
+ * @access   Private
+ */
+router.put('/', auth, async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('500 Internal server error');
+  }
+});
+
+/**
+ * @route    #reqtype: DELETE | #endpoint: api/posts
+ * @desc     test route
+ * @access   Private
+ */
+router.delete('/', auth, async (req, res) => {
+  try {
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('500 Internal server error');
+  }
+});
 
 module.exports = router;
