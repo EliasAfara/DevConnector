835752c888918b2b5059a608491f11d8af8f0065
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-10 19:57:50 +0200

    Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,46 +1,49 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
+import Spinner from '../layout/Spinner';
 
 const Dashboard = ({
   getCurrentProfile,
   auth: { user },
-  profile: { profile },
+  profile: { profile, loading },
 }) => {
   useEffect(() => {
     getCurrentProfile();
   }, [getCurrentProfile]);
 
-  return <div>Dashboard</div>;
-  // return (
-  //   <Fragment>
-  //     <h1 className="large text-primary">Dashboard</h1>
-  //     <p className="lead">
-  //       <i className="fas fa-user" /> Welcome {user && user.name}
-  //     </p>
-  //     {profile !== null ? (
-  //       <Fragment>
-  //         <DashboardActions />
-  //         <Experience experience={profile.experience} />
-  //         <Education education={profile.education} />
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <>
+          {/* <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} /> */}
 
-  //         <div className="my-2">
-  //           <button className="btn btn-danger" onClick={() => deleteAccount()}>
-  //             <i className="fas fa-user-minus" /> Delete My Account
-  //           </button>
-  //         </div>
-  //       </Fragment>
-  //     ) : (
-  //       <Fragment>
-  //         <p>You have not yet setup a profile, please add some info</p>
-  //         <Link to="/create-profile" className="btn btn-primary my-1">
-  //           Create Profile
-  //         </Link>
-  //       </Fragment>
-  //     )}
-  //   </Fragment>
-  // );
+          {/* <div className='my-2'>
+            <button className='btn btn-danger' onClick={() => deleteAccount()}>
+              <i className='fas fa-user-minus' /> Delete My Account
+            </button>
+          </div> */}
+        </>
+      ) : (
+        <>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create Profile
+          </Link>
+        </>
+      )}
+    </>
+  );
 };
 
 Dashboard.propTypes = {
