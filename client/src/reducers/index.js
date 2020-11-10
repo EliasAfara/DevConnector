5ebadb18ec99d11354fc4a5bc0993aa2cb7a86d6
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-10 16:26:42 +0200

    Profile Reducer & Get Current Profile

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
+import profile from './profile';
 
 export default combineReducers({
   // Will take in an object (whose values are reducers) that has all the reduces I create ex: auth reducer
   alert,
   auth,
+  profile,
 });
