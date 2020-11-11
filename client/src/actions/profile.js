1a5976517112f8990a2ed715be6ae9b26e08540f
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-11 19:24:16 +0200

    Profile Actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -30,9 +30,9 @@ export const getCurrentProfile = () => async (dispatch) => {
 
 // Get all profiles
 export const getAllProfiles = () => async (dispatch) => {
-  dispatch({
-    type: CLEAR_PROFILE,
-  });
+  // dispatch({
+  //   type: CLEAR_PROFILE,
+  // });
   try {
     const res = await axios.get('/api/profile');
 
