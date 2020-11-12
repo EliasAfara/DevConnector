fa1e0ea3a2fcad76ee4000c072962738b0b9e450
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-12 02:16:28 +0200

    Display both Post items & Post form

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,13 +3,30 @@ import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
+import PostForm from './PostForm';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return loading ? <Spinner /> : <>test</>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome to the community
+      </p>
+      <PostForm />
+      <div className='posts'>
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 Posts.propTypes = {
