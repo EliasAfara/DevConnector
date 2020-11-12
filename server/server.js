0905eb990d43cc0a1d5f97eb4767d160335a58a9
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-12 16:26:18 +0200

    Prepared for deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+// production
+const path = require('path');
+
 const app = express();
 
 // Connect Database
@@ -9,7 +12,7 @@ connectDB();
 // Init Middleware (Allow us to get the data from frontend)
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API RUNNING'));
+// app.get('/', (req, res) => res.send('API RUNNING'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/user'));
@@ -17,6 +20,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assests in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
